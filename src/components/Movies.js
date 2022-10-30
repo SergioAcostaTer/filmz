b4c0a7cdd45b43e0831515a81a59bc9c87853605
Sergio Acosta <sergioacostaquintana@gmail.com>
2022-10-30 19:29:44 +0000

    make it better

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,17 +7,19 @@ const Movies = () => {
 
   useEffect(() => {
     async function fetchData(){
-      if (!localStorage.trend) {
-        console.log(JSON.parse(localStorage.trend) === [])
-        const movies = await getTrendMoviesPics("day")
-        setMainMovies(movies)
-        console.log(movies)
-        localStorage.trend = JSON.stringify(movies);
-      }
-      if (localStorage.trend) {
-        // console.log(localStorage.trend)
-        setMainMovies(JSON.parse(localStorage.trend));
-      } 
+      const movies = await getTrendMoviesPics("day")
+      setMainMovies(movies)
+      // if (!localStorage.trend) {
+      //   console.log(JSON.parse(localStorage.trend) === [])
+      //   const movies = await getTrendMoviesPics("day")
+      //   setMainMovies(movies)
+      //   console.log(movies)
+      //   localStorage.trend = JSON.stringify(movies);
+      // }
+      // if (localStorage.trend) {
+      //   // console.log(localStorage.trend)
+      //   setMainMovies(JSON.parse(localStorage.trend));
+      // } 
     }
     fetchData()
   }, []); //eslint-disable-line
