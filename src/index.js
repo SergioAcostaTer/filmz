cb938a4802fd5653f25686c114d1c6c2d3a43a2a
Sergio Acosta <sergioacostaquintana@gmail.com>
2022-10-30 19:35:56 +0000

    make it better

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollTop";
 const root = createRoot(document.getElementById("root"));
-
 
 root.render(
   <BrowserRouter>
-    <App/>
+    <ScrollToTop>
+      <App/>
+    </ScrollToTop>
   </BrowserRouter>
 );
 // If you want to start measuring performance in your app, pass a function
